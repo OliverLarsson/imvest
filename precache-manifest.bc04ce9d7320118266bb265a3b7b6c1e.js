660fbadb55fb9bca49ffee3fc20c5c4081fee4ea
OliverLarsson <oliver.larsson@colorado.edu>
2020-03-05 11:56:21 -0700

    Updates

---
+++ precache-manifest.bc04ce9d7320118266bb265a3b7b6c1e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22a47bd4a5d7d66aa09062a57e81a48d",
+    "url": "/imvest/index.html"
+  },
+  {
+    "revision": "e7937983f8202585ec8f",
+    "url": "/imvest/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "ae67ea1871ae201653a2",
+    "url": "/imvest/static/css/main.83ca9be1.chunk.css"
+  },
+  {
+    "revision": "e7937983f8202585ec8f",
+    "url": "/imvest/static/js/2.aaa7ff52.chunk.js"
+  },
+  {
+    "revision": "df8be702f0cc9d299c0ff42ca03d3e40",
+    "url": "/imvest/static/js/2.aaa7ff52.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae67ea1871ae201653a2",
+    "url": "/imvest/static/js/main.31299b44.chunk.js"
+  },
+  {
+    "revision": "c045bb4c99a6e5651384",
+    "url": "/imvest/static/js/runtime-main.b8c43e8e.js"
+  }
+]);
