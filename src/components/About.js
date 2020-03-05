f7084a7c14eb0b4ab27e511bc3379ba2ab061b24
OliverLarsson <oliver.larsson@colorado.edu>
2020-03-05 12:08:19 -0700

    Test

--- src/components/About.js
+++ src/components/About.js
@@ -7,7 +7,7 @@ const About = () => (
       <main className="my-5 py-5">
         <Container className="px-0">
           <Row noGutters className="pt-2 pt-md-5 w-100 px-4 px-xl-0 position-relative">
-            <h1> About </h1>
+            <h1> About Change</h1>
           </Row>
         </Container>
       </main>
